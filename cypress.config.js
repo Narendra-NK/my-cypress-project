965ae6476eb85f504040bc01024944881ec067e5
Narendra-NK <narendrakumar.nalluri66@gmail.com>
2025-04-06 17:45:48 +0000

    Setup Cypress parallelization with cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
+const cypressSplit = require("cypress-split");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Use new ENV variable format for latest cypress-split
+      const splits = parseInt(config.env.SPLIT, 10);
+      const group = parseInt(config.env.SPLIT_INDEX, 10);
+
+      if (!splits || isNaN(group)) {
+        console.log("Invalid SPLIT or SPLIT_INDEX environment variables");
+        return config;
+      }
+
+      cypressSplit(on, config, { splits, group });
+      return config;
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,ts,jsx,tsx}",
+    supportFile: false,
   },
 });
